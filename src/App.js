919583bdb57f1b14b875a9446a3d398924958574
Joseph Lynn <josephlynn94@gmail.com>
2021-05-14 08:04:21 -0400

    finished button styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className='App'>
       <Button buttonStyle='btn--default'>Testing</Button>
+      <Button buttonStyle='btn--primary'>Testing</Button>
+      <Button buttonStyle='btn--secondary'>Testing</Button>
+      <Button buttonStyle='btn--danger'>Testing</Button>
+      <Button buttonStyle='btn--success'>Testing</Button>
+      <Button buttonStyle='btn--dark'>Testing</Button>
     </div>
   );
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,17 +5,10 @@ import './Button.css';
 const STYLES = [
   'btn--default',
   'btn--primary',
+  'btn--secondary',
   'btn--danger',
   'btn--success',
   'btn--dark',
-  'btn--light',
-  'btn--default--outline',
-  'btn--primary--outline',
-  'btn--warning--outline',
-  'btn--danger--outline',
-  'btn--success--outline',
-  'btn--dark--outline',
-  'btn--light--outline',
 ];
 const ICONS = [
   'arrow-down',
@@ -33,8 +26,6 @@ const Button = ({ children, type, disabled, onClick, buttonStyle, icon }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
-
-  const checkButtonIcon = ICONS.includes(icon) ? icon : '';
 
   return (
     <button
